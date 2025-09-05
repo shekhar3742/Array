1ef416254e219af3f5ea17dced7ef17025e7b339
shekhar3742 <imderek3742@gmail.com>
2025-09-05 22:10:52 +0530

    remove duplicates

---
+++ Easy/Q4.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int removeduplicates(int arr[], int n){
+    int i = 0;
+    for(int j=1; j<n; j++){
+        if(arr[i]!=arr[j]){
+            arr[i+1]= arr[j];
+            i++;
+        }
+
+    }
+   return i+1;
+}
+
+
+
+
+int main(){
+
+    int n;
+    cout<<"Enter size of array : "<<endl;
+    cin>>n;
+
+    int arr[n];
+    for(int i=1; i<=n ; i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    cout<<"Array :"<<endl;
+    for(int i=1; i<=n ; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+    cout<<removeduplicates(arr,n)<<endl;
+    return 0;
+
+}
