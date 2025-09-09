cfd3b5819a61c0316632db9eb0bfdf7a0af62a15
shekhar3742 <imderek3742@gmail.com>
2025-09-10 00:03:21 +0530

    Q7

--- Easy/Q7.cpp
+++ Easy/Q7.cpp
@@ -1,34 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
- void moveZeroes(int &arr[], int n) {
-        if(n>1){
-            int i = 0;
-              for (int j = 0; j < n; j++) {
-        if (arr[j] != 0) {
-            swap(arr[i], arr[j]);
-            i++;
-        }
+void moveZeroes(int arr[], int n)
+{
+    if (n > 1)
+    {
+        int i = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] != 0)
+            {
+                swap(arr[i], arr[j]);
+                i++;
             }
-        
+        }
     }
-    }
-int main(){
+}
+int main()
+{
     int n;
-    cout<<"Enter array size : "<<endl;
-    cin>>n;  
+    cout << "Enter array size : " << endl;
+    cin >> n;
     int arr[n];
-    
-    for(int i=0; i<n ; i++){
-        cin>>arr[i];
-    }
-    cout<<endl;
 
-    cout<<"Array : ";
-    for(int i=0; i<n ; i++){
-        cout<<arr[i]<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<endl;
-  
-    
+    cout << endl;
+
+    cout << "Array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    moveZeroes(arr, n);
+
+    cout << "Array after moving zeros : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
