30f55a163acadebf345a68d7c737c75134e3a60d
shekhar3742 <imderek3742@gmail.com>
2025-09-09 23:56:31 +0530

    Q7

---
+++ Easy/Q7.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+ void moveZeroes(int &arr[], int n) {
+        if(n>1){
+            int i = 0;
+              for (int j = 0; j < n; j++) {
+        if (arr[j] != 0) {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+            }
+        
+    }
+    }
+int main(){
+    int n;
+    cout<<"Enter array size : "<<endl;
+    cin>>n;  
+    int arr[n];
+    
+    for(int i=0; i<n ; i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+
+    cout<<"Array : ";
+    for(int i=0; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+  
+    
+}
